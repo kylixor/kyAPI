6507bc1ce607ea92f46994915a76e3c58571ba86
kylixor <93137659+kylixor@users.noreply.github.com>
2022-06-15 18:50:20 -0400

    Bar chart progress and CORS

--- main.go
+++ main.go
@@ -1,17 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type BarData struct {
+	Label           string `json:"label"`
+	BackgroundColor string `json:"backgroundColor"`
+	Data            []int  `json:"data"`
+}
+
+type GraphData struct {
+	Labels []string  `json:"labels"`
+	Bars   []BarData `json:"datasets"`
+}
+
 func helloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	fmt.Fprintf(w, "Hello World 2.0!")
+}
+
+func fakeData(w http.ResponseWriter, r *http.Request) {
+	// var data GraphData
+	// data.Labels = append(data.Labels, "January", "February", "March")
+	// var barData BarData
+	// barData.Label = "Wordle Score"
+	// barData.BackgroundColor = "#f87979"
+	// barData.Data = append(barData.Data, 40, 20, 12)
+	// data.Bars = append(data.Bars, barData)
+	newData := GraphData{
+		Labels: []string{"January", "February", "March"},
+		Bars: []BarData{
+			{
+				Label:           "Wordle Score",
+				BackgroundColor: "#f87979",
+				Data:            []int{40, 20, 12},
+			},
+		},
+	}
+	jsonResp, _ := json.Marshal(newData)
+	// w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write(jsonResp)
 }
 
 func main() {
 	http.HandleFunc("/", helloWorld)
+	http.HandleFunc("/fake-data", fakeData)
 	fmt.Println("Starting web server on 3000...")
 	if err := http.ListenAndServe(":3000", nil); err != nil {
 		log.Fatal(err)
